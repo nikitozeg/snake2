57a0c2db619cdc5e304726ae8b2b47772f629307
nikitozeg <tazg@ya.ru>
2014-01-27 01:46:12 +0400

    Moving algorithm fixed. (the turns worked not correctly)

--- src/RU/Board.java
+++ src/RU/Board.java
@@ -1,4 +1,4 @@
-package Snake2;
+package RU;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,13 +7,13 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.awt.Color;
-import java.util.Random;
 
 import static sun.swing.SwingUtilities2.drawString;
 
 public class Board extends JPanel implements ActionListener {
     int size = 10, delay = 150;
-    int x[] = new int[900], y[] = new int[900], dots, applex[]=new int[300], appley[]=new int[300];
+    boolean setter = true;
+    int x[] = new int[300], y[] = new int[300], dots = 3, applex[] = new int[300], appley[] = new int[300];
     boolean left = false, right = false, up = false, down = false;
     Timer timer;
     Image body, apple;
@@ -24,14 +24,15 @@ public class Board extends JPanel implements ActionListener {
         addKeyListener(new TAdaptaer());
         ImageIcon iid = new ImageIcon(this.getClass().getResource("dot.png"));
         body = iid.getImage();
-     //   ImageIcon apple = new ImageIcon(this.getClass().getResource("dot.png"));
-     //   apple = apple.getImage();
+        //   ImageIcon apple = new ImageIcon(this.getClass().getResource("dot.png"));
+        //   apple = apple.getImage();
         Start();
+
         setFocusable(true);
     }
 
     public void Start() {
-        dots = 3;
+
         for (int z = 0; z < dots; z++) {
             x[z] = 50 - z * 10;
             y[z] = 50;
@@ -41,28 +42,46 @@ public class Board extends JPanel implements ActionListener {
         timer.start();
     }
 
+    public void appleSett(Graphics g) {
+        //  super.paint(g);
+        g.drawImage(body, 10 * ((int) (Math.random() * 30)), 10 * ((int) (Math.random() * 30)), this);
+    }
+
+
     public void paint(Graphics g) {
         super.paint(g);
-        int a= (int) (Math.random()*10);
-        int b= (int) (Math.random()*10);
-        applex[1]=a;
-        appley[1]=b;
         for (int z = 0; z < dots; z++) {
             g.drawImage(body, x[z], y[z], this);
-          //  g.drawImage(body, applex[1], appley[1], this);
+
+            if (setter) {
+                setter = false;
+                appley[0] = 10 * ((int) (Math.random() * 27));
+                applex[0] = 10 * ((int) (Math.random() * 27));
+            }
+
+            g.drawImage(body, applex[0], appley[0], this);
+
+            //g.drawImage(body, applex[1], appley[1], this);
         }
     }
 
-    public void appleSet(){
-         int a= (int) Math.random()*10;
-         int b= (int) Math.random()*10;
-        applex[1]=a;
-        appley[1]=b;
+ /*   public void appleSet() {
+        applex[1] = (int) (Math.random() * 300);
+        appley[1] = (int) (Math.random() * 300);
 
+    }         */
 
+    public void checkApple() {
+        if ((x[0] == applex[0]) && (y[0] == appley[0])) {
+            dots++;
+            setter = true;
+        }
     }
+
     public void move() {
-        if (((x[1]==0)||(y[1]==0))==true) {
+
+
+        if (((x[0] == -10 || x[0] == 300) || (y[0] == -10 || y[0] == 300)) == true) {
             setBackground(Color.black);
             JOptionPane.showMessageDialog(null, "Meeting with the wall");
 
@@ -87,7 +106,8 @@ public class Board extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         move();
-       repaint();
+        checkApple();
+        repaint();
     }
 
     private class TAdaptaer extends KeyAdapter {
@@ -97,25 +117,25 @@ public class Board extends JPanel implements ActionListener {
             panel.add(label, BorderLayout.CENTER);
             label.setFont(new Font("Calibri", Font.PLAIN, 20));
             label.setText(e.getKeyText(e.getKeyCode()));   */
-            if ((e.getKeyCode()== KeyEvent.VK_LEFT)) {
+            if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (!right)) {
                 left = true;
                 up = false;
                 down = false;
             }
 
-            if ((e.getKeyCode() == KeyEvent.VK_RIGHT)) {
+            if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (!left)) {
                 right = true;
                 up = false;
                 down = false;
             }
 
-            if ((e.getKeyCode() == KeyEvent.VK_UP)) {
+            if ((e.getKeyCode() == KeyEvent.VK_UP) && (!down)) {
                 up = true;
                 right = false;
                 left = false;
             }
 
-            if ((e.getKeyCode() == KeyEvent.VK_DOWN)) {
+            if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (!up)) {
                 down = true;
                 right = false;
                 left = false;
